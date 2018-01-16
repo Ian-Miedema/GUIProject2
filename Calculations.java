2d0dd6d4b353e287e11454fdc08bb2606d735b1e
Ian-Miedema <32856223+Ian-Miedema@users.noreply.github.com>
2018-01-16 15:06:36 -0500

    Update Calculations.java

--- Calculations.java
+++ Calculations.java
@@ -5,12 +5,27 @@ package com.company;
  */
 public class Calculations {
 
-    //initialize the variables needed
-    private double dom1;
-    private double dom2;
-    private double g;
-    private double iterations;
+    //variables that define the f(x) equation
+    private double a_value;                 //the slope of the linear equation, the a-value of the quadratic equation, and the vertical stretch/compression of the sine and cosine functions
+    private double b_value;                 //the vertical shift of the linear equation, the b-value of the quadratic equation, the horizontal stretch/compression of the sine and cosine functions
+    private double c_value;                 //the c-value of the quadratic equation and the horizontal translation of the sine and cosine functions
+    private double d_value;                 //the vertical translation of the sine and cosine functions
+
+    //variables that act as restrictions
+    private double dom1;                    //the first domain
+    private double dom2;                    //the second domain
+    private double iterations;              //the variable used to gather the number of iterations wanted
+    private double g;                       //the variable used for the g(x) function
+
+    //variables used to calculate the area
+    private double area;                    //the variable used to add all of the rectangles together and then return the area
+    private double rect;                    //the variable used to calculate the area of each iteration
+    private double height;                  //the variable used to calculate the height
+    private double base;                    //the variable used to calculate the base
     private double volume;
+
+
+
 
     public void SetRestrictions (double d1, double d2, double gx, double slice){
         dom1 = d1;
@@ -102,6 +117,189 @@ public class Calculations {
         volume = 0;
     }
     //resets the volume so that if multiple calculations occur they do not accumulate and skew results
+
+
+    //------------------------------------------------------------------------------------------------------------------
+
+    //Methods for Area
+
+    public double CalcAreaBetween (double a, double b) {
+        SetLineData(a, b);
+        calcAreaLinear();
+        return area;
+    }
+    //calculates the are underneath a linear function and returns the area
+
+    public double CalcAreaBetween (double a, double b, double c) {
+        SetQuadraticData(a, b, c);
+        calcAreaQuadratic();
+        return area;
+    }
+    //calculates the area underneath a quadratic function and returns the area
+
+    public double CalcAreaBetween (EnumeratedType f, double a, double b, double c, double d) {
+        SetSinusoidalData(a, b, c, d);
+        if (f == EnumeratedType.SINE){
+            calcAreaSine();
+        }//if the value chosen was 3 then the user wanted to use a sine function
+        else {
+            calcAreaCosine();
+        }//if the value chosen was not 3 then the user wanted to use a cosine function because the only other option is 4
+        return area;
+        //returns the calculated area
+    }
+    //calculates the area underneath a sinusoidal function and returns the area
+
+    private void SetLineData (double a, double b) {
+        a_value = a;
+        b_value = b;
+    }
+    //sets the linear equation values
+
+    private void SetQuadraticData (double a, double b, double c) {
+        a_value = a;
+        b_value = b;
+        c_value = c;
+    }
+    //sets the quadratic equation values
+
+    private void SetSinusoidalData (double a, double b, double c, double d) {
+        a_value = a;
+        b_value = b;
+        c_value = c;
+        d_value = d;
+    }
+    //sets the sinusoidal equation values
+
+    private double calcAreaLinear () {
+
+        height = a_value*dom1 + b_value - g;
+        //finds the y-value at the beginning of the domain
+
+        base = (dom2 - dom1) / iterations;
+        //find how big each base there are
+
+        for (int x = 0; x < iterations; x++){
+            //how many rectangle/slice area to calculate
+            //restricts number of areas to find
+
+            rect = Math.abs(height*base);
+            //calculates the area of the rectangle by multiplying the base, which is calculated  by the height, the y-value
+
+            dom1 += base;
+            //finds the x-value for the next iteration
+
+            height = a_value*dom1 + b_value - g;
+            //recalculates the y-value based on the new x-value
+
+            area += rect;
+            //adds all of the areas of the rectangles together
+        }//end for loop
+        return area;
+        //returns the area to the main class
+    }
+    //calculates the area under a linear function
+
+    private double calcAreaQuadratic () {
+
+        height = a_value*(dom1 - b_value)*(dom1 - b_value) + c_value - g;
+        //finds the y-value at the beginning of the domain
+
+        base = (dom2 - dom1) / iterations;
+        //this finds the width of the slices
+
+        for (int i = 0; i < iterations; i++) {
+            //how many rectangle/slice area to calculate
+            //restricts number of areas to find
+
+            rect = Math.abs(height * base);
+            //calculates the area of the rectangle
+
+            dom1 += base;
+            //moves on to the next iteration
+
+            height = a_value*(dom1 - b_value)*(dom1 - b_value) + c_value - g;
+            //recalculates the base of the new iteration
+
+            area += rect;
+            //adds all of the areas of the rectangles together to get the total area
+        }//end of for loop
+        return area;
+        //returns the total area
+    }
+    //calculates the area under a quadratic function
+
+    private double calcAreaSine () {
+
+        height = a_value * Math.sin((b_value * (dom1 - c_value))) + d_value - g;
+        //finds the y-value at the beginning of the domain
+
+
+        base = (dom2 - dom1) / iterations;
+        //this finds the width of the slices
+
+
+        for (int i = 0; i < iterations; i++) {
+            //how many rectangle/slice area to calculate
+            //restricts number of areas to find
+
+            rect = Math.abs(height * base);
+            //calculates the area of the rectangle
+
+            dom1 += base;
+            //moves on to the next iteration
+
+            height = a_value * Math.sin((b_value * (dom1 - c_value))) + d_value - g;
+            //recalculates the base of the new
+
+
+            area += rect;
+            //adds all of the areas of the rectangles together to get the total area
+        }//end of for loop
+        return area;
+        //returns the total area
+    }
+    //calculates the area under a sine function
+
+    private double calcAreaCosine (){
+
+        height = a_value * Math.cos((b_value * (dom1 - c_value))) + d_value - g;
+        //finds the y-value at the beginning of the domain
+
+        base = (dom2 - dom1) / iterations;
+        //this finds the width of the slices
+
+        for (int i = 0; i < iterations; i++) {
+            //how many rectangle/slice area to calculate
+            //restricts number of areas to find
+
+            rect = Math.abs(height * base);
+            //calculates the area of the rectangle
+
+            dom1 += base;
+            //moves on to the next iteration
+
+            height = a_value * Math.cos((b_value * (dom1 - c_value))) + d_value - g;
+            //recalculates the base of the new
+
+            area += rect;
+            //adds all of the areas of the rectangles together to get the total area
+        }//end of for loop
+        return area;
+        //returns the total area
+    }
+    //calculates the area under a cosine function
+
+    public void resetData () {
+
+        area = 0;
+        rect = 0;
+        height = 0;
+        base = 0;
+
+    }
+    //resets all of the variables in the class
+
 
 }//end of class
 
